d86ff3d07a0ed443179a293acbd8b9e2cbe7bcc4
mohammed.el.khelyfy <mElKhelyfy@e-lma.com>
2024-02-04 18:53:46 +0100

    second commit

---
+++ src/lib_managment/user.java
@@ -0,0 +1,19 @@
+package lib_managment;
+
+public class user {
+	private String name;
+	private String idNumber;
+	
+	public String getIdNumber() {
+		return idNumber;
+	}
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
